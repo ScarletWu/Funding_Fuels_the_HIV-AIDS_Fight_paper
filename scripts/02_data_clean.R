f17d77f78335e896b9a4559b7487058de187ea68
“ScarletWu” <scarletruoxianwu@gmail.com>
2024-04-17 15:54:14 +0000

    cleaned data

--- scripts/02_data_clean.R
+++ scripts/02_data_clean.R
@@ -16,3 +16,7 @@ data$fem_resp_age <- as.factor(data$fem_resp_age)
 # Ensure no missing values in key variables
 data_filtered <- data %>%
   filter(complete.cases(data[, c("ind_fem_depression_change", "ind_fem_tired_change", "ind_fem_worried_change", "ind_fem_safety_change", "mental_index_change", "geo_state", "fem_resp_age")]))
+
+# Save the cleaned data to a CSV file
+cleaned_data_file <- "/cloud/project/data/cleaned/cleaned_data.csv"
+write.csv(data_filtered, file = cleaned_data_file, row.names = FALSE)
