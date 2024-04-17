58d31c846f1bbebf22f9d0e08daf8d3b09e6e21c
“ScarletWu” <scarletruoxianwu@gmail.com>
2024-04-17 15:49:24 +0000

    packages

--- scripts/00_package_install.R
+++ scripts/00_package_install.R
@@ -1,4 +1,18 @@
-install.packages('haven')
-install.packages('ggplot2')
-install.packages('dplyr')
-install.packages('fixest')
+# Install and load each package
+install_packages("haven")
+load_packages("haven")
+
+install_packages("dplyr")
+load_packages("dplyr")
+
+install_packages("ggplot2")
+load_packages("ggplot2")
+
+install_packages("glmnet")
+load_packages("glmnet")
+
+install_packages("broom")
+load_packages("broom")
+
+install_packages("MASS")
+load_packages("MASS")
